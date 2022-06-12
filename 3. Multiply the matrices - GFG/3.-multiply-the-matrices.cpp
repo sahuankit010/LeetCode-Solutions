5f47901d0ecafbeea5402b5b1e6769e57e7f983e
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-06-12 03:05:50 -0500

    Added solution - LeetHub

---
+++ 3. Multiply the matrices - GFG/3.-multiply-the-matrices.cpp
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+
+
+class Solution
+{   
+    public:
+    //Function to multiply two matrices.
+    vector<vector<int> > multiplyMatrix( const vector<vector<int> >& A, const vector<vector<int> >& B)
+    {
+        // code here
+        if(A[0].size()!=B.size()) return {{-1}};
+         vector<vector<int>> ans;
+         for(int i = 0; i < A.size();i++){
+           vector<int> arr;
+           for(int j = 0; j < B[0].size(); j++ ){
+               int sum = 0;
+               for(int k = 0; k< A[0].size(); k++)
+                   sum += A[i][k] * B [k][j];
+               arr.push_back(sum);
+           }
+           ans.push_back(arr);
+       }
+       return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > A(row); 
+        for(int i=0; i<row; i++)
+        {
+            A[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>A[i][j];
+            }
+        }
+        cin>> row>> col;
+        vector<vector<int> > B(row);
+        for(int i=0; i<row; i++)
+        {
+            B[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>B[i][j];
+            }
+        }
+        Solution ob;
+        vector<vector<int> > result = ob.multiplyMatrix(A,B);
+
+        if(result.size() == 0)
+            cout<<"-1";
+        else
+        {
+            for (int i = 0; i < result.size(); ++i)
+            {
+                for (int j = 0; j < result[0].size(); ++j)
+                {
+                    cout<<result[i][j]<<" ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
