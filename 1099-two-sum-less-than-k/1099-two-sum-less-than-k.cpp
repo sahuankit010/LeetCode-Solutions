09cb31560004dae602660e30a74082d6dd7bcf8f
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-06-15 19:12:10 -0500

    Time: 11 ms (21.49%), Space: 9.1 MB (28.93%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cpp
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int twoSumLessThanK(vector<int>& arr, int k) {
+        
         sort(arr.begin(),arr.end());
         int maxSum=INT_MIN;
         
@@ -8,32 +9,11 @@ public:
         
         while(low<high){
             int currSum = arr[low]+arr[high];
-            // int mid =    low + (high-low)/2;
-                // low + (high-low)/2;
-            
-            
             if(currSum<k and currSum>maxSum){
-                // int temp = maxSum;
                 maxSum = currSum;
             }
-            
             else if(currSum<k) low++;
-            
             else high--;
-            
-            
-            
-            
-            
-            
-//             if(arr[currSum]<k and arr[currSum]>maxSum){
-//                 // int temp = maxSum;
-//                 maxSum = arr[currSum];
-//             }
-            
-//             else if(arr[currSum]<k) low = currSum + 1;
-            
-//             else high = currSum - 1;
         }
         if(maxSum!=INT_MIN) return maxSum;
         return -1;
