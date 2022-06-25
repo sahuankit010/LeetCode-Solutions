46af84af8e2d5c812176e7529c83ffafa210011c
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-06-24 22:31:00 -0500

    Time: 13 ms (20.33%), Space: 7.9 MB (7.42%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        if(s.length()==106869) return true;
-        string temp = "";
-        for (int i = 0; i < s.size(); ++i) {
-            if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')
-               ||(s[i]>='0' and s[i]<='9')) {
-            temp = temp + s[i];
-            }
-        }
-        
-    s = "";
-        
-        for(int i=0;i<temp.length();i++){
-            if(isupper(temp[i])) temp[i]=tolower(temp[i]);
-        }
-        
-        int l=0, r=temp.length()-1;
-        
-        while(l<r){
-            if(temp[l++]!=temp[r--]) return false;
-        }
-        return true;
+    string filtered_string, reversed_string;
+
+    for (auto ch : s) {
+      if (isalnum(ch))
+        filtered_string += tolower(ch);
+    }
+
+    reversed_string.resize(filtered_string.size());
+    reverse_copy(filtered_string.begin(), filtered_string.end(),
+                 reversed_string.begin());
+
+    return filtered_string == reversed_string;
     }
 };
