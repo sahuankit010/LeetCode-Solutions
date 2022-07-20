20e24c6f04ae17638cd409d45e40a6ffca9ec2ee
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-07-20 04:22:35 -0500

    Time: 12 ms (94.59%), Space: 13.2 MB (81.69%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-       int rows = matrix.size(), cols = matrix[0].size();
-  vector < int > dummy1(rows,-1), dummy2(cols,-1);
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < cols; j++) {
-      if (matrix[i][j] == 0) {
-        dummy1[i] = 0;
-        dummy2[j] = 0;
-      }
-    }
-
-  }
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < cols; j++) {
-      if (dummy1[i] == 0 || dummy2[j]==0) {
-        matrix[i][j] = 0;
-      }
-    }
-  }
+        
+        int sizeR=matrix.size(), sizeC=matrix[0].size();
+        vector<int>row(sizeR,-1), col(sizeC,-1);
+        
+        for(int i=0;i<matrix.size();i++)
+            for(int j=0;j<matrix[i].size();j++)
+                if(matrix[i][j]==0){
+                    row[i]=0;
+                    col[j]=0;
+                }
+            
+        for(int i=0;i<sizeR;i++)
+            for(int j=0;j<sizeC;j++)
+                if(row[i]==0 or col[j]==0)
+                    matrix[i][j]=0;
     }
 };
