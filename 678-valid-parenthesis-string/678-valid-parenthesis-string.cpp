384ec666578be834cc1b1456b0ff8fc653759fac
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-08-16 05:24:14 -0500

    Time: 3 ms (46.03%), Space: 6.1 MB (83.30%) - LeetHub

---
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> st, star;
+        
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(') st.push(i);
+            else if(s[i]=='*') star.push(i);
+            else{
+                if(!st.empty()) st.pop();
+                else if(!star.empty()) star.pop();
+                else return false;
+            }
+        }
+        
+        while(!st.empty() && !star.empty()){
+            if(st.top()>star.top()) return false;
+            st.pop();
+            star.pop();
+        } 
+        return st.empty();
+    }
+};
