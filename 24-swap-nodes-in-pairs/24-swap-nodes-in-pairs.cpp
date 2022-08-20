bebac132d20478a1ebb9eaeb32c10c0e7ff53ec6
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-08-20 18:06:42 -0500

    Time: 0 ms (100.00%), Space: 7.6 MB (7.26%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -11,11 +11,21 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        ListNode *temp = head;
-        while(temp!=nullptr && temp->next!=nullptr){
-            swap(temp->val, temp->next->val);
-            temp=temp->next->next;
+       // changing pointers
+        if(head == nullptr || head->next == nullptr)  return head;
+        ListNode* curr = head->next->next;
+        ListNode* prev = head;
+        head = head->next;
+        head->next = prev;
+        while(curr!=nullptr && curr->next!=NULL){
+            prev->next = curr->next;
+            prev = curr;
+            ListNode* next = curr->next->next;
+            curr->next->next = curr;
+            curr = next;
         }
+        
+        prev->next = curr;
         return head;
     }
 };
