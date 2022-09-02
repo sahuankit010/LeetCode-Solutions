365cb1381dd2a698c952c40ae7c3f05934a3a915
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-09-02 13:16:24 -0500

    Time: 0 ms (100.00%), Space: 5.8 MB (88.98%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    unsigned long int binomialCoeff(unsigned int n, unsigned int k){
+        unsigned long int res = 1;
+        if (k > n - k)  k = n - k;
+        for (int i = 0; i < k; ++i){
+            res *= (n - i);
+            res /= (i + 1);
+        }
+        return res;
+    }
+    unsigned long int catalan(unsigned int n){
+    unsigned long int c = binomialCoeff(2*n, n);
+    return c/(n+1);
+    }
+ 
+    int numTrees(int n) {
+        unsigned long int count = catalan(n);
+        return count;
+    }
+};
