0b1b8f5e2591b8c7ea110ceb5bc1212554c2679a
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-09-11 02:48:17 -0500

    Time: 205 ms (21.49%), Space: 61.9 MB (44.07%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -11,19 +11,34 @@
  */
 class Solution {
 public:
-    int height(TreeNode* root){
-        if(!root) return 0;
-        return 1 + max(height(root->left), height(root->right));
-    }
-    int inOrder(TreeNode* root,int h){
-        if(!root) return 0;
-        if(h==1) return root->val;
-        return inOrder(root->left, h-1) + inOrder(root->right, h-1);
-    }
+    // int height(TreeNode* root){
+    //     if(!root) return 0;
+    //     return 1 + max(height(root->left), height(root->right));
+    // }
+    // int inOrder(TreeNode* root,int h){
+    //     if(!root) return 0;
+    //     if(h==1) return root->val;
+    //     return inOrder(root->left, h-1) + inOrder(root->right, h-1);
+    // }
     int deepestLeavesSum(TreeNode* root) {
-        int h = height(root);
-        cout<<h<<endl;
-        int sum = inOrder(root, h);
+        if(!root) return 0;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        int sum = 0;
+        while(!q.empty()){
+            int n = q.size();
+            sum = 0; // re-initialize sum, if another level exists
+            while(n--){
+                TreeNode *node = q.front();q.pop();
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+                if(!node->right && !node->left) // if this is a leaf node
+                    sum += node->val;
+            }
+        }
         return sum;
     }
 };
