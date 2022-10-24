01fb3132eea9813908e21daf702db73e80cc1af4
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-10-23 21:52:44 -0500

    Time: 0 ms (100.00%), Space: 7.7 MB (77.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    int robFunction(int i, vector<int>nums, vector<int> &memo){
-        if(i>=nums.size()) return 0;
+//     int robFunction(int i, vector<int>nums, vector<int> &memo){
+//         if(i>=nums.size()) return 0;
         
-        if(memo[i]!=-1){
-            return memo[i];
-        }
+//         if(memo[i]!=-1){
+//             return memo[i];
+//         }
         
-        int ans = max(robFunction(i+1, nums, memo), robFunction(i+2, nums, memo)+nums[i]);
-        memo[i]=ans;
-        return  ans;
-    }
+//         int ans = max(robFunction(i+1, nums, memo), robFunction(i+2, nums, memo)+nums[i]);
+//         memo[i]=ans;
+//         return  ans;
+//     }
     int rob(vector<int>& nums) {
-        vector<int>memo(101, -1);
-        return  robFunction(0, nums, memo);
+        int n = nums.size();
+        
+        vector<int>dp(n+1);
+        
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=1; i<n; i++)
+            dp[i+1] = max(dp[i-1]+nums[i], dp[i]);
+        return dp[n];
+        // return  robFunction(0, nums, memo);
     }
 };
