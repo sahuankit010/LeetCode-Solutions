1af8813fb6c9e569f5e07ff1232d4d2766eba22b
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-10-28 01:44:15 -0500

    Time: 4 ms (84.47%), Space: 8.4 MB (22.49%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.cpp
+++ 1854-maximum-population-year/1854-maximum-population-year.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        map<int,int> ump;
+//         map<int,int> ump;
+//         for(int i=0; i<logs.size(); i++){
+//             for(int j=logs[i][0]; j<logs[i][1]; j++){
+//                 ump[j]++;
+//             }
+//         }
+//         int maxi = -1;
+//         int ans = -1;
         
-        for(int i=0; i<logs.size(); i++){
-            for(int j=logs[i][0]; j<logs[i][1]; j++){
-                ump[j]++;
-            }
+//         for(auto x: ump){
+//             if(maxi<x.second){
+//                 ans = x.first;
+//                 maxi = x.second;
+//             }
+//         }
+//         return ans;
+        
+        vector<int>vec(2051,0);
+        
+        for(int i=0;i<logs.size(); i++){
+            vec[logs[i][0]] += 1;
+            vec[logs[i][1]] -= 1;
         }
         
-        int maxi = -1;
-        int ans = -1;
+        for(int i=2; i<2051; i++){
+            vec[i] += vec[i-1];
+        }
         
-        for(auto x: ump){
-            if(maxi<x.second){
-                ans = x.first;
-                maxi = x.second;
+        int maxi = -1, ans=-1;
+        
+        for(int i=1; i<2051; i++){
+            if(maxi<vec[i]){
+                maxi = vec[i];
+                ans = i;
             }
         }
         
