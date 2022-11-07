15abc2bcab2ace0120d2f9a77cac2f9da8d1e0c5
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-11-07 16:56:20 -0600

    Time: 16 ms (7.04%), Space: 9.2 MB (41.78%) - LeetHub

---
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countElements(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        int n=nums.size();
+        for(int i=1; i<n; i++){
+            int j=i-1;
+            while(j>=0 && nums[j]>=nums[i]) j--;
+            int k=i+1;
+            while(k<n && nums[k]<=nums[i]) k++;
+            cout<<j<<" "<<k<<endl;
+            if(j>=0 && k<n) ans++;
+        }
+        
+        return ans;
+    }
+};
