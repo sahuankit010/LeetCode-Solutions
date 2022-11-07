d1f49bf7b4845f3c6da1e6e229904621c15ba067
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-11-07 01:36:30 -0600

    Time: 393 ms (6.80%), Space: 59.6 MB (66.28%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -5,22 +5,22 @@ public:
         if(n<3) return false;
         // cout<<n<<endl;
         
-        for(int i=0; i<n-1; i++){
-            if(nums[i]>nums[i+1])
-                return i;
-        }
-        return -1;
+        // for(int i=0; i<n-1; i++){
+        //     if(nums[i]>nums[i+1])
+        //         return i;
+        // }
+        // return -1;
 
-//         int l=0, h=n-1;
+        int l=0, h=n-1;
         
-//         while(l<=h){
-//             int mid = l+(h-l)/2;
-//             int curr = nums[mid];
-//             if(curr>nums[mid-1] && curr<nums[mid+1]) return mid;
-//             else if(curr<nums[mid-1]) l=mid+1;
-//             else h=mid-1;
-//         }
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            int curr = nums[mid];
+            if(curr>nums[mid-1] && curr>nums[mid+1]) return mid;
+            else if(curr>nums[mid-1] && curr<nums[mid+1]) l=mid;
+            else h=mid;
+        }
         
-//         return INT_MAX;
+        return INT_MAX;
     }
 };
