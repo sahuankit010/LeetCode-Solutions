7dbc72d28a2e25f377ce66b4f34a3a4ac1638e26
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-11-07 16:58:39 -0600

    Time: 38 ms (7.04%), Space: 9.1 MB (41.78%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -6,9 +6,13 @@ public:
         int n=nums.size();
         for(int i=1; i<n; i++){
             int j=i-1;
-            while(j>=0 && nums[j]>=nums[i]) j--;
+            while(j>=0 && nums[j]>=nums[i]){
+                j--;
+            }
             int k=i+1;
-            while(k<n && nums[k]<=nums[i]) k++;
+            while(k<n && nums[k]<=nums[i]){
+               k++;
+            }
             cout<<j<<" "<<k<<endl;
             if(j>=0 && k<n) ans++;
         }
