d1937271360b488240ebf059a2f39ac8482d0a9a
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-11-16 02:27:12 -0600

    Time: 255 ms (5.74%), Space: 22.4 MB (48.95%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        
+        int n= arr.size();
+        if(n<3) return false;
+        
+        int i=1;
+        bool inc = false,dec = false;
+        for(; i<n; i++){
+            cout<<i<<endl;
+            if(arr[i]==arr[i-1]) return false;
+            if(arr[i]<arr[i-1]) break;
+        }
+        inc = i>=2;
+        if(i==n-1){
+            dec = arr[n-1]<arr[n-2];
+        }
+        for(; i<n-1; i++){
+            cout<<i<<endl;
+            dec=true;
+            if(arr[i]==arr[i+1]) return false;
+            if(arr[i+1]>arr[i]) break;
+        }
+        return (i>=n-1) && dec && inc;
+    }
+};
