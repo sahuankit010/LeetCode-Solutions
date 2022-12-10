3740237bc5a1e7dd232a480a4cfedf7fc2cc76ba
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-12-10 01:39:25 -0600

    Time: 1246 ms (54.39%), Space: 6 MB (23.98%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum=1;
+        if(num==1) return false;
+        for(int i=2; i<=num/2; i++){
+            if(num%i==0){
+                sum += i;
+            }
+        }
+        
+        return sum == num;
+    }
+};
