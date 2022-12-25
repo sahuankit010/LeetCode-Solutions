d88a6944a3aee25e21c53bd41fb33767243ac4a3
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2022-12-24 21:36:31 -0600

    Time: 49 ms (5.04%), Space: 43.2 MB (77.45%) - LeetHub

---
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -0,0 +1,49 @@
+class Solution {
+    
+    static int binarySearch(int [] prefix, int target) {
+        System.out.println("I just got executed!");
+        
+        int l=0, r = prefix.length-1;
+        int ans=-1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            
+            if(prefix[mid]==target) return ans = mid;
+            else if(prefix[mid]>target && mid>l && prefix[mid-1]<target) {
+                ans = mid-1;
+                r=mid-1;
+            }
+            
+            else if(prefix[mid]<target) {
+                ans = mid;
+                l = mid+1;
+            }
+            else r = mid-1;
+        }
+        return ans;
+  }
+    public int[] answerQueries(int[] nums, int[] queries) {
+        int n = nums.length;
+        int m = queries.length;
+        Arrays.sort(nums);
+        
+        int []ans =  new int[m];
+        int []prefix = new int[n];
+        
+        prefix[0] = nums[0];
+        
+        for(int i=1; i<n; i++){
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+        // for(int i=0; i<n; i++){
+        //     System.out.print(prefix[i]+ " ");
+        // }
+        // System.out.println();
+        for(int i=0; i<m; i++){
+            int idx = binarySearch(prefix, queries[i]);
+            ans[i] = idx == -1? 0: idx+1;
+        }
+        
+        return ans;
+    }
+}
