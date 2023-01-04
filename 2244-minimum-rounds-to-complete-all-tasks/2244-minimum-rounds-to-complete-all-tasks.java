53732c4147d47b31708095bbfc688ab562e89687
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2023-01-03 19:43:42 -0600

    Time: 101 ms (20.87%), Space: 112.1 MB (8.00%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -3,19 +3,11 @@ class Solution {
         Arrays.sort(tasks);
         HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
         int ans = 0;
-        for(int i=0; i<tasks.length; i++){
-            Integer c = hmap.get(tasks[i]);
-            if (hmap.get(tasks[i]) == null)
-               hmap.put(tasks[i], 1);
-
-            // If elements already exists in hash map
-            else 
-              hmap.put(tasks[i], ++c);
-        }
+        for(int i=0; i<tasks.length; i++)
+            hmap.put(tasks[i], hmap.getOrDefault(tasks[i], 0) + 1);
         
         for(int i=0; i<tasks.length; ){
             int count = hmap.get(tasks[i]);
-            
             if(count>=2){
                 if(count<=5 && count%2==0 && count%3!=0){
                     hmap.put(tasks[i], count-2);
