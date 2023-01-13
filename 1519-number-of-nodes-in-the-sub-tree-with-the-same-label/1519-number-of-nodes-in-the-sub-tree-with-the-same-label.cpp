3a76ac8532af751a842ece8249e752868c07b1c7
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2023-01-13 01:33:18 -0600

    Time: 663 ms (75.00%), Space: 244.1 MB (24.28%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,44 +1,30 @@
 class Solution {
 public:
-    vector<int> dfs(vector<vector<int>>&g,string &s,vector<int>&vis,int val,vector<int> &a){
-		//We have a temp vector to keep the account of char in the sub trees
-        vector<int>temp(26);
-		//Making the visited node true or marking it visited
-        vis[val]=1;
-		
-		//Now we start Traversing the neighboring nodes of the val node.
-        for(int neig:g[val]){
-		//We will call dfs only for the node which are not visited
-            if(!vis[neig]){
-                vector<int> child= dfs(g,s,vis,neig,a);
-				
-				//adding the label to the temp vector
-                temp[s[neig]-'a']++;
-				
-				//adding the labels of all the subtree labels
-                for(int i=0;i<26;i++){
-                    temp[i]+=child[i];
-                }
+    vector<int> DFS(unordered_map<int, vector<int>> &adj,
+                   int curr, int parent, vector<int> &res, string &labels){
+        
+        vector<int> myCount(26, 0);
+        char myLabel = labels[curr];
+        myCount[myLabel-'a'] = 1;
+        for(int &v: adj[curr]){
+            if(v==parent) continue;
+            vector<int> child = DFS(adj, v, curr, res, labels);
+            
+            for(int i=0; i<26; i++){
+                myCount[i]+= child[i];
             }
         }
-		//Finally adding temp value to the ans vector.
-        a[val]+=temp[s[val]-'a']+1;
-        return temp;
+        res[curr] = myCount[myLabel - 'a'];
+        return myCount;
     }
-    vector<int> countSubTrees(int n, vector<vector<int>>& e, string labels) {
-        vector<vector<int>>g(n);
-		//First step is to convert the edges into the graph
-		//so as given undirected graph so we have made 2d vector for graph
-
-        for(vector<int> v : e){
-            g[v[0]].push_back(v[1]);
-            g[v[1]].push_back(v[0]);
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        unordered_map<int, vector<int>> adj;
+        for(vector<int> &v: edges){
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
         }
-		//Second step is to make the visited vector 
-		//inorder to keep the count of the node that has been visited.
-        vector<int>vis(n),ans(n),temp(26);
-        dfs(g,labels,vis,0,ans);
-        return ans;
+        vector<int> res(n,0);
+        DFS(adj, 0, -1, res, labels);
+        return res;
     }
-
 };
