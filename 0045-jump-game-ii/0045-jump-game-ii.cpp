ca92636b04321ce024eae126a124fb239858bbc9
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2023-02-07 19:12:12 -0600

    Time: 19 ms (69.69%), Space: 16.6 MB (84.26%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
-    long long int solve(vector<int> &dp, vector<int> &nums, int curr, int des){
-        if(curr==des) return 0;
-        if(dp[curr]!=-1) return dp[curr];
-        long long int tmp = INT_MAX;
-        for(int i=1; i<= nums[curr]; i++){
-            if(i+curr>des) break;
+//     long long int solve(vector<int> &dp, vector<int> &nums, int curr, int des){
+//         if(curr==des) return 0;
+//         if(dp[curr]!=-1) return dp[curr];
+//         long long int tmp = INT_MAX;
+//         for(int i=1; i<= nums[curr]; i++){
+//             if(i+curr>des) break;
             
-            tmp = min(tmp, 1 + solve(dp, nums, curr+i, des));
-        }
-        return dp[curr] = tmp;
-    }
+//             tmp = min(tmp, 1 + solve(dp, nums, curr+i, des));
+//         }
+//         return dp[curr] = tmp;
+//     }
     int jump(vector<int>& nums) {
         int n =  nums.size();
-        vector<int> dp(n,-1);
-        return solve(dp, nums, 0, n-1);
+        
+        if(n==1) return 0;
+        int jump = 1, currjump = nums[0], maxjump = nums[0];
+        int i=1;
+        while(i<n-1){
+            maxjump = max(maxjump, i + nums[i]);
+            
+            if(i == currjump){
+                jump++;
+                currjump = maxjump;
+            }
+            i++;
+        }
+        return jump;
     }
 };
