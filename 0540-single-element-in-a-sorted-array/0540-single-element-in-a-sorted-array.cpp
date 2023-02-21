a0e60ba1c1ccb884c86e70af42885e1b58bec972
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2023-02-20 20:40:32 -0600

    Time: 56 ms (9.99%), Space: 27.5 MB (6.23%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,10 +3,14 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return nums[0];
-        int ans = nums[0];
-        for(int i=1; i<n; i++){
-            ans ^= nums[i];
+        unordered_map<int,int> ump;
+        
+        for(int i=0; i<n; i++){
+            ump[nums[i]]++;
         }
-        return ans;
+        for(auto x: ump){
+            if(x.second == 1) return x.first;
+        }
+        return -1;
     }
 };
