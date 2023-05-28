bf3737c0a93562b2156d100fe48846104b79ac5c
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2023-05-28 04:24:11 -0500

    Time: 9 ms (72.62%), Space: 7.3 MB (82.52%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int arr[26]={0};
+        unordered_map<char, int> ump;
         
         for(char c: s){
-            arr[c - 97]++;
-        }
-        for(char c: t){
-            arr[c - 97]--;
+            ump[c]++;
         }
         
-        for(int i=0; i<26; i++){
-            if(arr[i]!=0) return false;
+        for(char c: t){
+            ump[c]--;
+        }
+        
+        for(auto it: ump){
+            if(it.second) return false;
         }
         
         return true;
