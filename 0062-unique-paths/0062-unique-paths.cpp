544b7b13f018a87d4d53001b2fb3bd8f5286f8b1
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2023-09-03 02:13:50 -0500

    Time: 0 ms (100.00%), Space: 6.7 MB (6.60%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int f(int i, int j, int m, int n, vector<vector<int>> &dp){
         // if
-        if(i==m || j>=n) return 0;
+        if(i==m || j==n) return 0;
         
         if(i==m-1 || j==n-1) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
