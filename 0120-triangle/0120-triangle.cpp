85855d416759cdbf937cb455015e5057c8cc7480
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2023-09-18 00:09:37 -0500

    Time: 6 ms (45.06%), Space: 9.4 MB (6.64%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int f(int i, int j, int m, vector<vector<int>>& triangle, vector<vector<int>> &dp){
+        if(i == m-1) return triangle[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j] = triangle[i][j] + min(f(i+1, j, m, triangle, dp), f(i+1, j+1, m, triangle, dp));
+    }
+    
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        int m = triangle.size();
+        vector<vector<int>> dp(m, vector<int>(m, -1));
+        return f(0, 0, m, triangle, dp);
+    }
+};
