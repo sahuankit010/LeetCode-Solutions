7f0e639df0ecb41fa1139ddc94f8e2faac3e2ce3
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2023-09-18 02:43:52 -0500

    Time: 8 ms (76.19%), Space: 10.9 MB (9.15%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    static bool comp(pair<int, int> &p1, pair<int, int> &p2){
-        if(p1.second < p2.second) return true;
-        return p1.second == p2.second? p1.first <= p2.first : false;
-    }
+
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         int m = mat.size();
         int n= mat[0].size();
         vector<pair<int, int>> store(m);
         for(int i=0; i<m; i++){
-            int count1 = 0;
-            for(int j=0; j<n; j++){
-                if(mat[i][j]) count1++;
-            }
-            store[i] = {i, count1};
+            store[i] = {accumulate(begin(mat[i]), end(mat[i]), 0),i};
+            
+             // = {count1,i};
         }
         
-        sort(begin(store), end(store), comp);
+        sort(begin(store), end(store));
         
         vector<int> ans(k);
         
         for(int i=0; i<k; i++)
-            ans[i] = store[i].first;
+            ans[i] = store[i].second;
         
         return ans;
     }
