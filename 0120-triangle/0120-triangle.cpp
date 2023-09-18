fbecf661bb66f8829c28be470bcaa32344bc957a
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2023-09-18 00:52:19 -0500

    Time: 4 ms (77.20%), Space: 9.4 MB (6.64%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -10,6 +10,20 @@ public:
         
         int m = triangle.size();
         vector<vector<int>> dp(m, vector<int>(m, -1));
-        return f(0, 0, m, triangle, dp);
+        // return f(0, 0, m, triangle, dp);
+        
+        for(int i=0; i<triangle[m-1].size(); i++)
+            dp[m-1][i] = triangle[m-1][i];
+        
+        for(int i=m-2; i>=0; i--){
+            
+            for(int j=i; j>=0; j--){
+                
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+                
+            }
+        }
+        
+        return dp[0][0];
     }
 };
