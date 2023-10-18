f997eb63af03798c60ad39bc5b0dd2f2eb5a3c5e
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2023-10-18 17:33:54 -0500

    Time: 83 ms (16.38%), Space: 46 MB (16.35%) - LeetHub

--- 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
@@ -2,46 +2,61 @@ class Solution {
 public:
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
 
+        // Create an adjacency list to represent the tree structure.
         unordered_map<int, vector<int>> adjList;
+        
+        // Create a mapping to track the parent of each child node.
         unordered_map<int, int> childToParent;
 
+        // Build the adjacency list and child-to-parent mapping.
         for(int i=0; i<n; i++){
             int leftC = leftChild[i];
             int rightC = rightChild[i];
 
-            if(leftC!=-1){
+            if(leftC != -1){
                 adjList[i].push_back(leftC);
-                if(childToParent.find(leftC)!=childToParent.end()){
+                
+                // Check if a child already has a parent; return false if so.
+                if(childToParent.find(leftC) != childToParent.end()){
                     return false;
                 }
+                
+                // Update the parent for the left child.
                 childToParent[leftC] = i;
             }
 
-            if(rightC!=-1){
+            if(rightC != -1){
                 adjList[i].push_back(rightC);
-                if(childToParent.find(rightC)!=childToParent.end()){
+                
+                // Check if a child already has a parent; return false if so.
+                if(childToParent.find(rightC) != childToParent.end()){
                     return false;
                 }
+                
+                // Update the parent for the right child.
                 childToParent[rightC] = i;
             }
         }
-        cout<<"1\n";
+        
         int root = -1;
 
-        for(int i =0; i<n; i++){
+        // Find the root node of the binary tree.
+        for(int i = 0; i < n; i++){
 
+            // If a node has no parent, and we haven't found a root yet, set it as the root.
             if(childToParent.find(i) == childToParent.end()){
-                if(root != -1) return false;
-                root  = i;
+                if(root != -1) return false; // If we already found a root, return false.
+                root = i;
             }
         }
-        cout<<"2\n";
-        if(root==-1) return false;
-        cout<<"3\n";
-
+        
+        // If no root node was found, return false.
+        if(root == -1) return false;
+        
+        // Perform a breadth-first traversal to ensure that all nodes are connected.
         vector<bool> visited(n, false);
         queue<int> q;
-
+        
         int count = 1;
         q.push(root);
         visited[root] = true;
@@ -58,8 +73,8 @@ public:
                 }
             }
         }
-        cout<<"4\n";
-        return count == n;
         
+        // Check if all nodes were reached during traversal.
+        return count == n;
     }
 };
