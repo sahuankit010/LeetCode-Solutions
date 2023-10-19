a8ccffbbb4a260277ede7ba119645d052450b68e
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2023-10-19 02:50:00 -0500

    Time: 17 ms (30.94%), Space: 9.3 MB (44.56%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -24,8 +24,25 @@ public:
         int n1 = word1.size();
         int n2 = word2.size();
 
-        vector<vector<int>> memo(n1+1, vector<int> (n2+1, -1));
+        vector<vector<int>> dp(n1+1, vector<int> (n2+1, 0));
 
-        return f(word1, word2, n1, n2, memo);
+        // return f(word1, word2, n1, n2, memo);
+
+        for(int i=0; i<n1+1; i++)
+        dp[i][0] = i;
+
+        for(int i=0; i<n2+1; i++)
+        dp[0][i] = i;
+
+        for(int i=1; i<n1+1; i++){
+            for(int j=1; j<n2+1; j++){
+                if(word1[i-1]==word2[j-1])
+                dp[i][j] = dp[i-1][j-1];
+                else{
+                    dp[i][j] = 1 + min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[n1][n2];
     }
 };
