07a609949ac643684980ae1a661ae9ded27b87df
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2023-10-19 02:13:58 -0500

    Time: 9 ms (66.58%), Space: 9.6 MB (20.78%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    int recur(string &word1, string &word2, int l1, int l2, vector<vector<int>> &memo){
-        if(l1 == 0) return l2;
-        if(l2 == 0) return l1;
-        if(memo[l1][l2]!=-1) return memo[l1][l2];
-        if(word1[l1-1] == word2[l2-1]) 
-            return memo[l1][l2] = recur(word1, word2, l1-1, l2-1, memo);
-        else{
-            int ins = recur(word1, word2, l1, l2-1, memo);
-            int del = recur(word1, word2, l1-1, l2, memo);
-            int rep = recur(word1, word2, l1-1, l2-1, memo);
-            
-            return memo[l1][l2] = 1 + min(ins, min(del, rep));
+// converting word1 to word2
+// through insertion, deletion, and substitution
+    int f(string &word1, string &word2, int n1, int n2, vector<vector<int>> &memo){
+
+        if(n1==0) return n2;
+        if(n2==0) return n1;
+
+        if(memo[n1][n2]!=-1) return memo[n1][n2];
+
+        if(word1[n1-1] == word2[n2-1]) return f(word1, word2, n1-1, n2-1, memo);
+
+        else {
+            int insertion = f(word1, word2, n1, n2 - 1, memo);
+            int deletion = f(word1, word2, n1 - 1, n2, memo);
+            int substitution = f(word1, word2, n1 - 1, n2 - 1, memo);
+            return memo[n1][n2] = 1 + min(insertion, min(deletion, substitution));
         }
+        // return 1 + min(f(word1, word2, n1-1, n2), min(f(word1, word2, n1, n2-1), f(word1, word2, n1-1, n2-1)));
     }
     int minDistance(string word1, string word2) {
-        int m = word1.size(), n = word2.size();
-        vector<vector<int>> memo(m+1, vector<int>(n+1, -1));
-        return recur(word1, word2, m, n, memo);
+
+        int n1 = word1.size();
+        int n2 = word2.size();
+
+        vector<vector<int>> memo(n1+1, vector<int> (n2+1, -1));
+
+        return f(word1, word2, n1, n2, memo);
     }
 };
