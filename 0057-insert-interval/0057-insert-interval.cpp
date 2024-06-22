ffe0192c3b052f3ffd31f020a94e109f981b6042
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2024-06-22 18:43:42 -0500

    Time: 13 ms (48.94%), Space: 20.6 MB (44.9%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> ans;
         
         int n = intervals.size();
-        if(n==0) {
-            ans.push_back(newInterval);
-            return ans;
-        }   
-        for (int i = 0; i < intervals.size(); i++){
-            if (intervals[i][1] < newInterval[0]){
-                ans.push_back(intervals[i]);
-            }
-			// the new interval's range is before the other, so we can add the new interval and update it to the current one
-            else if (intervals[i][0] > newInterval[1])
-            {
-                ans.push_back(newInterval);
-                newInterval = intervals[i]; 
-            }
-			// the new interval is in the range of the other interval, we have an overlap, so we must choose the min for start and max for end of interval 
-            else if(intervals[i][1] >= newInterval[0] || intervals[i][0] <= newInterval[1])
-            {
-                newInterval[0] = min(intervals[i][0], newInterval[0]);
-                newInterval[1] = max(newInterval[1], intervals[i][1]);
+        int i = 0;
+        vector<vector<int>> ans;
 
-            }
+        while(i<n && intervals[i][1]< newInterval[0]){
+            ans.push_back(intervals[i]);
+            i++;
         }
-        ans.push_back(newInterval); 
+
+        while(i<n && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+
+        ans.push_back(newInterval);
+
+        while(i<n){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+
         return ans;
+
     }
 };
