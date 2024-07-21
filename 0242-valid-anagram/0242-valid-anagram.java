dac211e67b8312566f6b36450bf4bfd10d2e2a44
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2024-07-20 19:13:31 -0500

    Time: 4 ms (70.32%), Space: 42.9 MB (81.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int [] arr = new int[26];
-        
+        int n1 = s.length();
+        int n2 = t.length();
+
+        if(n1 != n2) return false;
+
+        int[] arr = new int[26];
+
+        for(int i=0; i<n1; i++){
+            arr[s.charAt(i) -'a']++;
+            arr[t.charAt(i) -'a']--;
+        }
+
         for(int i=0; i<26; i++){
-            System.out.print(arr[i] + " ");
+            if(arr[i] != 0) return false;
         }
-        char[] sarr = s.toCharArray();
-        char[] tarr = t.toCharArray();
-        for(char c: sarr){
-            arr[c-97]++;
-        }
-        
-        for(char c: tarr){
-            arr[c-97]--;
-        }
-        
-        for(int i=0; i<26; i++){
-            if(arr[i]!=0) return false;
-        }
+
         return true;
     }
 }
