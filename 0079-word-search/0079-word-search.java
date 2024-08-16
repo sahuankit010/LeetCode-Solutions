b2dd30c5dbff89c9d85c7d56c84c8e6ab5048d54
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2024-08-16 16:07:02 -0500

    Time: 138 ms (68.9%), Space: 41.2 MB (86.62%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
-        boolean[][] vis = new boolean[m][n];
+        // boolean[][] vis = new boolean[m][n];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0) && dfs(board, word, 0, i, j, vis)) {
+                if (board[i][j] == word.charAt(0) && dfs(board, word, 0, i, j)) {
                     return true;
                 }
             }
@@ -15,7 +15,7 @@ class Solution {
         return false;
     }
 
-    private boolean dfs(char[][] board, String word, int idx, int x, int y, boolean[][] vis) {
+    private boolean dfs(char[][] board, String word, int idx, int x, int y) {
         if (idx == word.length()) {
             return true;
         }
@@ -23,18 +23,18 @@ class Solution {
         int m = board.length;
         int n = board[0].length;
 
-        if (x < 0 || x >= m || y < 0 || y >= n || vis[x][y] || board[x][y] != word.charAt(idx)) {
+        if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] == '*' || board[x][y] != word.charAt(idx)) {
             return false;
         }
+        char c = board[x][y];
+        board[x][y] = '*';
 
-        vis[x][y] = true;
-
-        boolean found = dfs(board, word, idx + 1, x + 1, y, vis) ||
-                        dfs(board, word, idx + 1, x - 1, y, vis) ||
-                        dfs(board, word, idx + 1, x, y + 1, vis) ||
-                        dfs(board, word, idx + 1, x, y - 1, vis);
-
-        vis[x][y] = false;  // backtrack
+        boolean found = dfs(board, word, idx + 1, x + 1, y) ||
+                        dfs(board, word, idx + 1, x - 1, y) ||
+                        dfs(board, word, idx + 1, x, y + 1) ||
+                        dfs(board, word, idx + 1, x, y - 1);
+        // vis[x][y] = false;  
+        board[x][y] = c; // backtrack
 
         return found;
     }
