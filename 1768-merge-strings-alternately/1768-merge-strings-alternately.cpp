134227bf41acce7f0cb8df541dabb076feb98750
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2024-09-24 00:08:52 -0500

    Time: 4 ms (22.11%), Space: 7.9 MB (78.36%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int n1 = word1.size(), n2 = word2.size();
-        
+        int size1 = word1.size(), size2 = word2.size();
+
         string ans = "";
-        
-        int i=0, j=0;
-        
-        while(i<n1 && j<n2){
-            ans += word1[i];
-            ans += word2[j];
-            i++;
-            j++;
+
+        int i = 0, j=0; // iterators for word1 and word2
+        int count = 0;
+        while(i<size1 && j<size2){
+            if(count % 2 == 0){
+                ans += word1[i++];
+            }else{
+                ans += word2[j++];
+            }
+            count++;
         }
-        
-        while(i<n1){
-            ans += word1[i];
-            i++;
-        }
-            
-        while(j<n2){
-            ans += word2[j];
-            j++;
-        }
-        
+
+        (i==size1)? addRemainingString(word2, j, size2, ans):
+                    addRemainingString(word1, i, size1, ans);
+
         return ans;
     }
+
+    private:
+        void addRemainingString(string &word, int index, int size, string &ans){
+            while(index<size){
+                ans+=word[index++];
+            }
+        }
 };
