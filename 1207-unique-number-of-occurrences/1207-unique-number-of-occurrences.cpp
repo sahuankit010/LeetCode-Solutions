2f02a34aafd5379b45c06134dfbe5ff840cf661c
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2024-10-05 21:01:47 -0500

    Time: 0 ms (100%), Space: 11.5 MB (42.9%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int> ump;
-        for(int i=0; i<arr.size(); i++){
-            ump[arr[i]]++;
+        unordered_map<int, int> hashmap;
+
+        for(int i: arr){
+            hashmap[i]++;
         }
-        unordered_set<int> us;
-        
-        for(auto x: ump){
-            us.insert(x.second);
+        unordered_set<int> seen;
+        for(pair<int, int> count: hashmap){ // {key, val}
+            if(seen.count(count.second)) return false;
+            seen.insert(count.second);
         }
-        
-        return ump.size()==us.size();
+        return true;
+        // TC: O(n) (with two traversal)
+        // SC: O(n) (hashset and hashmap)
     }
 };
