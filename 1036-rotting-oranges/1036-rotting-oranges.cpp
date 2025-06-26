cdf79a7af1547538d9a88809ad83ff22a381c2f1
Ankit Sahu <32036503+sahuankit010@users.noreply.github.com>
2025-06-25 21:02:56 -0500

    Time: 3 ms (30.51%), Space: 16.7 MB (88.57%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,57 +1,71 @@
 class Solution {
 public:
+    int m, n;
 
     int orangesRotting(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
 
-        int m = grid.size();
-        int n = grid[0].size();
+        int freshOranges = 0, rottenOranges = 0;
 
-        queue<pair<int,int>> q;
+        queue<pair<int, int>> q;
 
-        int minutes = -1, freshOranges = 0;
-
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-
-                if(grid[i][j] == 2) q.push({i, j});
-                    
-                if(grid[i][j] == 1) freshOranges++;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    freshOranges++;
+                } else if (grid[i][j] == 2) {
+                    rottenOranges++;
+                    q.push({i, j});
+                }
             }
         }
 
-        if(freshOranges == 0) return 0;
+        if (freshOranges == 0)
+            return 0;
 
-        if(q.empty()) return -1;
+        if (q.empty())
+            return -1;
 
-        int dirs[][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int minutes = -1;
 
-        while(!q.empty()){
-            
+        while (!q.empty()) {
             int size = q.size();
+            while (size--) {
+                pair<int, int> p = q.front();
 
-            while(size--){
-                
-                auto it  = q.front();
                 q.pop();
 
-                for(int k = 0; k<4; k++){
+                int dirs[][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-                    int x = dirs[k][0] + it.first;
-                    int y = dirs[k][1] + it.second;
+                for (int k = 0; k < 4; k++) {
+                    int xx = dirs[k][0] + p.first;
+                    int yy = dirs[k][1] + p.second;
 
-                    if((x >= 0 && x < m && y >= 0 && y < n) && grid[x][y] == 1){
+                    if (check(grid, xx, yy)) {
                         freshOranges--;
-                        q.push({x, y});
-                        grid[x][y] = 2;
-                    
+                        grid[xx][yy] = 2;
+                        q.push({xx, yy});
                     }
                 }
             }
-            
+
             minutes++;
         }
 
-        if(!freshOranges) return minutes;
-        return -1;
+        if (freshOranges != 0)
+            return -1;
+
+        return minutes;
+    }
+
+    bool check(vector<vector<int>>& grid, int xx, int yy) {
+        if (xx < 0 || xx == m || yy < 0 || yy == n)
+            return false;
+
+        if (grid[xx][yy] != 1)
+            return false;
+
+        return true;
     }
 };
